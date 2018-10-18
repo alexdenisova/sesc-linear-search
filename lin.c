5a38568a3faceed1c579c39afac5599d8d3897d0
Alex Denisova <alexadenisova@gmail.com>
2018-10-18 18:22:45 +0700

    linear-search

---
+++ lin.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i =0, len, k, first = -1, last = -1, n;
+	scanf("%d", &n);
+	scanf("%d", &k);
+	int arr[n];
+	for(i=0; i<n; i++){
+		scanf("%d", &arr[i]);
+	}
+  for(i=0; i<n; i++){
+    if((first==-1) && (arr[i]==k)){
+      first = i;
+    }
+    if((first!=-1) && (arr[i]==k)){
+      last = i;
+    }
+  }
+	printf("%d %d", first, last);
+	
+
+    return 0;
+}
